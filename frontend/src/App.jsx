ba12d666d03251511fa7736bd155fddc59e84e50
Olufemi <olufemifatunwase@gmail.com>
2026-08-28 01:51:07 +0100

    Add login route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -12,15 +12,26 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<ProtectedRoute>
-          <Dashboard />
-          </ProtectedRoute>} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />}  />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-      </Routes>
+  <Route path="/" element={<Navigate to="/login" replace />} />
+  <Route path="/login" element={<Login />} />
+  <Route
+    path="/dashboard"
+    element={
+      <ProtectedRoute>
+        <Dashboard />
+      </ProtectedRoute>
+    }
+  />
 
+  <Route path="/register" element={<Register />} />
+
+  <Route path="/forgot-password" element={<ForgotPassword />} />
+
+  <Route
+    path="/reset-password/:token"
+    element={<ResetPassword />}
+  />
+</Routes>
       <ToastContainer
         position="top-right"
         autoClose={3000}
